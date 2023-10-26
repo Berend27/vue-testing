c7a40ced57705b0e341068b32dd1a50764cbb8ab
solarkdexter <dexterb@sol-ark.com>
2023-10-26 11:38:17 -0500

    finished the 'Your First Test' tutorial

--- tests/unit/AppHeader.spec.js
+++ tests/unit/AppHeader.spec.js
@@ -1,10 +1,17 @@
+import { mount } from "@vue/test-utils";
+import AppHeader from "@/components/AppHeader";
+
 describe("AppHeader", () => {
   test("if a user is not logged in, do not show the logout button", () => {
-    // test body
+    const wrapper = mount(AppHeader);
+    expect(wrapper.find("button").isVisible()).toBe(false);
   });
 
-  test("if a user is logged in, show the logout button", () => {
-    // test body
+  // async await was used, unlike in the tutorial
+  test("if logged in, show logout button", async () => {
+    const wrapper = mount(AppHeader);
+    await wrapper.setData({ loggedIn: true });
+    expect(wrapper.find("button").isVisible()).toBe(true);
   });
   // it() can be used instead of test()
 });
