7da24ba746a2d39cd112d45c9e6c13cce34d0fd5
Dexter Berend <dexterjberend@gmail.com>
2023-10-26 05:40:52 -0500

    test stubs

---
+++ tests/unit/AppHeader.spec.js
@@ -0,0 +1,10 @@
+describe("AppHeader", () => {
+  test("if a user is not logged in, do not show the logout button", () => {
+    // test body
+  });
+
+  test("if a user is logged in, show the logout button", () => {
+    // test body
+  });
+  // it() can be used instead of test()
+});
